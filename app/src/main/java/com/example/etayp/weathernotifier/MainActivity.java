358e5fed05d43b2dcae8f189145afcad7aa61a53
Dima <dmitrorl@gmail.com>
2017-08-19 14:32:05 +0300

    version 1.5.3 visual changes

--- app/src/main/java/com/example/etayp/weathernotifier/MainActivity.java
+++ app/src/main/java/com/example/etayp/weathernotifier/MainActivity.java
@@ -19,8 +19,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -91,14 +89,6 @@ public class MainActivity extends AppCompatActivity implements
 
 //        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         if (findViewById(R.id.fragment_container) != null) {
             if (savedInstanceState != null) {
